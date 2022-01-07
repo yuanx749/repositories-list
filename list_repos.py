8db18f1bf852ed5a4a7c7e669d8eadbd27811811
yuanx749 <yuanx749@gmail.com>
2022-01-07 08:03:41 +0100

    Update pages

--- list_repos.py
+++ list_repos.py
@@ -15,11 +15,13 @@ response = urllib.request.urlopen(url)
 repo_lst = json.loads(response.read().decode("utf-8"))
 
 # %%
+repo_lst_1 = [repo for repo in repo_lst if not repo["fork"] and repo["homepage"]]
+repo_lst_2 = [repo for repo in repo_lst if not repo["fork"] and not repo["homepage"]]
 with open("README.md", "w") as f_:
     f_.write("# Repositories List\n")
     f_.write("An automatically updated list of my public non-forked repositories.\n")
-    for repo in repo_lst:
-        if not repo["fork"]:
-            f_.write(
-                f"- [{repo['name']}]({repo['html_url']}) - {repo['description']}\n"
-            )
+    for repo in repo_lst_1:
+        f_.write(f"- [{repo['name']}]({repo['homepage']}) - {repo['description']}\n")
+    f_.write("\n")
+    for repo in repo_lst_2:
+        f_.write(f"* [{repo['name']}]({repo['html_url']}) - {repo['description']}\n")
