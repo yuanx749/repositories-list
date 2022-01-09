796b9d0c59c9264d332c82393af1058f8afb49d5
yuanx749 <yuanx749@gmail.com>
2022-01-09 09:56:51 +0100

    Sort by commit time and add PAT
    GitHub does not clean commits. Delete this repo and create a new one.

--- list_repos.py
+++ list_repos.py
@@ -1,5 +1,7 @@
 # %%
+import datetime
 import json
+import os
 import urllib.parse
 import urllib.request
 
@@ -15,8 +17,25 @@ response = urllib.request.urlopen(url)
 repo_lst = json.loads(response.read().decode("utf-8"))
 
 # %%
-repo_lst_1 = [repo for repo in repo_lst if not repo["fork"] and repo["homepage"]]
-repo_lst_2 = [repo for repo in repo_lst if not repo["fork"] and not repo["homepage"]]
+repo_source_lst = [repo for repo in repo_lst if not repo["fork"]]
+dt_lst = []
+github_token = os.environ.get("GITHUB_TOKEN", "")
+for repo in repo_source_lst:
+    url = f"https://api.github.com/repos/{user}/{repo['name']}/commits"
+    if github_token:
+        header = {"Authorization": f"token {github_token}"}
+        req = urllib.request.Request(url, headers=header)
+    else:
+        req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    commit_lst = json.loads(response.read().decode("utf-8"))
+    dt = commit_lst[-1]["commit"]["author"]["date"]
+    dt_lst.append(datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ"))
+dt_lst, repo_source_lst = zip(*sorted(zip(dt_lst, repo_source_lst), reverse=True))
+
+# %%
+repo_lst_1 = [repo for repo in repo_source_lst if repo["homepage"]]
+repo_lst_2 = [repo for repo in repo_source_lst if not repo["homepage"]]
 with open("README.md", "w") as f_:
     f_.write("# Repositories List\n")
     f_.write("An automatically updated list of my public non-forked repositories.\n")
