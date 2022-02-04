38a961a8511fcb41dd7a13618b813b7284a9c703
yuanx749 <yuanx749@gmail.com>
2022-02-04 10:37:25 +0100

    Classify with topics

--- list_repos.py
+++ list_repos.py
@@ -15,11 +15,12 @@ params = urllib.parse.urlencode(
 url = f"https://api.github.com/users/{user}/repos?{params}"
 response = urllib.request.urlopen(url)
 repo_lst = json.loads(response.read().decode("utf-8"))
+repo_source_lst = [repo for repo in repo_lst if not repo["fork"]]
 
 # %%
-repo_source_lst = [repo for repo in repo_lst if not repo["fork"]]
-dt_lst = []
+# get more requests per hour using authentication
 github_token = os.environ.get("GITHUB_TOKEN", "")
+dt_lst = []
 for repo in repo_source_lst:
     url = f"https://api.github.com/repos/{user}/{repo['name']}/commits"
     if github_token:
@@ -31,16 +32,26 @@ for repo in repo_source_lst:
     commit_lst = json.loads(response.read().decode("utf-8"))
     dt = commit_lst[-1]["commit"]["author"]["date"]
     dt_lst.append(datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ"))
-dt_lst, repo_source_lst = zip(*sorted(zip(dt_lst, repo_source_lst), reverse=True))
+# only sort by time, dict does not support sort
+dt_lst, repo_source_lst = zip(
+    *sorted(zip(dt_lst, repo_source_lst), key=lambda e: e[0], reverse=True)
+)
 
 # %%
-repo_lst_1 = [repo for repo in repo_source_lst if repo["homepage"]]
-repo_lst_2 = [repo for repo in repo_source_lst if not repo["homepage"]]
-with open("README.md", "w") as f_:
-    f_.write("# Repositories List\n")
-    f_.write("An automatically updated list of my public non-forked repositories.\n")
-    for repo in repo_lst_1:
+tags = ["project", "package"]
+f_ = open("README.md", "w")
+f_.write("# Repositories List\n")
+f_.write("An automatically updated list of my public non-forked repositories.\n")
+f_.write("\n### Projects\n")
+for repo in repo_source_lst:
+    if "project" in repo["topics"]:
         f_.write(f"- [{repo['name']}]({repo['homepage']}) - {repo['description']}\n")
-    f_.write("\n")
-    for repo in repo_lst_2:
-        f_.write(f"* [{repo['name']}]({repo['html_url']}) - {repo['description']}\n")
+f_.write("\n### Packages\n")
+for repo in repo_source_lst:
+    if "package" in repo["topics"]:
+        f_.write(f"* [{repo['name']}]({repo['homepage']}) - {repo['description']}\n")
+f_.write("\n### Others\n")
+for repo in repo_source_lst:
+    if all(tag not in repo["topics"] for tag in tags):
+        f_.write(f"+ [{repo['name']}]({repo['html_url']}) - {repo['description']}\n")
+f_.close()
