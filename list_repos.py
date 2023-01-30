cbe1832f861aff2558a5f95bb54f7d42f3282d26
yuanx749 <yuanx749@gmail.com>
2023-01-30 23:57:40 +0800

    Add forked repos

--- list_repos.py
+++ list_repos.py
@@ -6,6 +6,20 @@ import urllib.parse
 import urllib.request
 
 # %%
+# get more requests per hour using authentication
+github_token = os.environ.get("GITHUB_TOKEN", "")
+header = {}
+if github_token:
+    header["Authorization"] = f"Bearer {github_token}"
+
+
+def query(url):
+    req = urllib.request.Request(url, headers=header)
+    response = urllib.request.urlopen(req)
+    return json.loads(response.read().decode("utf-8"))
+
+
+# %%
 user = "yuanx749"
 params = urllib.parse.urlencode(
     {
@@ -13,45 +27,41 @@ params = urllib.parse.urlencode(
     }
 )
 url = f"https://api.github.com/users/{user}/repos?{params}"
-response = urllib.request.urlopen(url)
-repo_lst = json.loads(response.read().decode("utf-8"))
-repo_source_lst = [repo for repo in repo_lst if not repo["fork"]]
+repo_lst = query(url)
 
 # %%
-# get more requests per hour using authentication
-github_token = os.environ.get("GITHUB_TOKEN", "")
 dt_lst = []
-for repo in repo_source_lst:
+for repo in repo_lst:
     url = f"https://api.github.com/repos/{user}/{repo['name']}/commits"
-    if github_token:
-        header = {"Authorization": f"token {github_token}"}
-        req = urllib.request.Request(url, headers=header)
-    else:
-        req = urllib.request.Request(url)
-    response = urllib.request.urlopen(req)
-    commit_lst = json.loads(response.read().decode("utf-8"))
+    commit_lst = query(url)
     dt = commit_lst[-1]["commit"]["author"]["date"]
     dt_lst.append(datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ"))
 # only sort by time, dict does not support sort
-dt_lst, repo_source_lst = zip(
-    *sorted(zip(dt_lst, repo_source_lst), key=lambda e: e[0], reverse=True)
-)
+dt_lst, repo_lst = zip(*sorted(zip(dt_lst, repo_lst), key=lambda e: e[0], reverse=True))
+source_repo_lst = [repo for repo in repo_lst if not repo["fork"]]
+fork_repo_lst = [repo for repo in repo_lst if repo["fork"]]
 
 # %%
 tags = ["project", "package", "private"]
 f_ = open("README.md", "w")
 f_.write("# Repositories List\n")
-f_.write("An automatically updated list of my public non-forked repositories.\n")
+f_.write("An automatically updated list of my public repos and repos contributed to.\n")
 f_.write("\n### Projects\n")
-for repo in repo_source_lst:
+for repo in source_repo_lst:
     if "project" in repo["topics"]:
         f_.write(f"- [{repo['name']}]({repo['homepage']}) - {repo['description']}\n")
 f_.write("\n### Packages\n")
-for repo in repo_source_lst:
+for repo in source_repo_lst:
     if "package" in repo["topics"]:
         f_.write(f"* [{repo['name']}]({repo['homepage']}) - {repo['description']}\n")
 f_.write("\n### Others\n")
-for repo in repo_source_lst:
+for repo in source_repo_lst:
     if all(tag not in repo["topics"] for tag in tags):
         f_.write(f"+ [{repo['name']}]({repo['html_url']}) - {repo['description']}\n")
+f_.write("\n### Contributions\n")
+for repo in fork_repo_lst:
+    url = f"https://api.github.com/repos/{user}/{repo['name']}"
+    repo_dict = query(url)
+    src = repo_dict["source"]
+    f_.write(f"- [{src['full_name']}]({src['html_url']}) - {src['description']}\n")
 f_.close()
